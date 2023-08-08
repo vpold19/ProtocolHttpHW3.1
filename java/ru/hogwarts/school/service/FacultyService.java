1657d6e5de01a7fab9e02e5973ad025ed601c55e
vadim <vadimpoldolin19@gmail.com>
2023-08-08 02:30:10 +0200

    finished

--- java/ru/hogwarts/school/service/FacultyService.java
+++ java/ru/hogwarts/school/service/FacultyService.java
@@ -2,6 +2,7 @@ package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.DataNotFoundedException;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
@@ -10,35 +11,35 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class StudentService {
-    private final Map<Long, Student> map = new HashMap<>();
+public class FacultyService {
+    private final Map<Long, Faculty> map = new HashMap<>();
     private Long COUNTER = 1L;
-    public Collection<Student> getByAge(int age){
+    public Collection<Faculty> getByColor(String color){
         return map.values().stream().
-                filter(student -> student.getAge()==age).
+                filter(faculty -> faculty.getColor().equalsIgnoreCase(color)).
                 collect(Collectors.toList());
     }
 
-    public Student getById(Long id){
+    public Faculty getById(Long id){
         return map.get(id);
     }
-    public Collection<Student> getAll(){
+    public Collection<Faculty> getAll(){
         return map.values();
     }
-    public Student create(Student student){
+    public Faculty create(Faculty faculty){
         Long nextId = COUNTER++;
-        student.setId(nextId);
-        map.put(student.getId(), student);
-        return student;
+        faculty.setId(nextId);
+        map.put(faculty.getId(), faculty);
+        return faculty;
     }
-    public Student update(Long id, Student student){
+    public Faculty update(Long id, Faculty faculty){
         if (!map.containsKey(id)){
             throw new DataNotFoundedException();
         }
-        Student existingStudent = map.get(id);
-        existingStudent.setName(student.getName());
-        existingStudent.setAge(student.getAge());
-        return existingStudent;
+        Faculty existingFaculty = map.get(id);
+        existingFaculty.setName(faculty.getName());
+        existingFaculty.setColor(faculty.getColor());
+        return existingFaculty;
     }
     public void delete(Long id){
         if (map.remove(id)==null){
